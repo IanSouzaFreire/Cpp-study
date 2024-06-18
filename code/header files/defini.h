31d7d07357adfac45089724fcbad6c78c6f866a9
Ian Souza Freire <iantmessenger+@gmail.com>
2024-06-18 19:41:33 -0300

    add header

---
+++ code/header files/defini.h
@@ -0,0 +1,15 @@
+#ifndef DEFINI_H
+#define DEFINI_H
+
+namespace deff {
+    
+class Data {
+public:
+  int id;
+  char *name;
+  char *data[]; // Data file 
+};
+
+}
+
+#endif
---
+++ code/header files/main.cpp
@@ -0,0 +1,12 @@
+#include "defini.h"
+
+int main() {
+  deff::Data mydata;
+  
+  mydata.id = 1;
+  mydata.name = (char*) "vincie";
+  mydata.data[0] = (char*) "dfile.dat";
+  mydata.data[1] = (char*) "b";
+  
+  return 0;
+}
